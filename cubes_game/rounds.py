d31b989f35917c854af08162f974eae735dc0afa
snckmykek <bukreev.sp@mail.ru>
2020-02-13 21:34:23 +0300

    minimum workable assembly

--- cubes_game/rounds.py
+++ cubes_game/rounds.py
@@ -10,17 +10,17 @@ class SingleRound:
 
 # First round
 first_round = SingleRound()
-first_round.swipes = 10
+first_round.swipes = 30
 first_round.colors = 3
 
 # Second round
 second_round = SingleRound()
-second_round.swipes = 12
+second_round.swipes = 20
 second_round.colors = 4
 
 # Third round
 third_round = SingleRound()
-third_round.swipes = 20
+third_round.swipes = 40
 third_round.colors = 2
 
 rounds = [first_round, second_round, third_round]
